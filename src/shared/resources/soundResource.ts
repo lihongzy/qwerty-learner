3e84d2efaa5fc01a3340cdf725bc73e116c59d87
lihongzy <l1810638451@gmail.com>
2026-06-20 13:40:26 +0800

    fix:修复非根路径部署时音频资源加载失败

--- src/shared/resources/soundResource.ts
+++ src/shared/resources/soundResource.ts
@@ -1,60 +1,59 @@
-import { LanguageType } from '@/shared/types/resource'
-import { PronunciationType } from '../types'
-
+import { LanguageType } from '@/shared/types/resource';
+import { PronunciationType } from '../types';
 
 export type PronunciationConfig = {
-  name: string
-  pron: PronunciationType
-}
+  name: string;
+  pron: PronunciationType;
+};
 export type LanguagePronunciationMapConfig = {
-  defaultPronIndex: number
-  pronunciation: PronunciationConfig[]
-}
+  defaultPronIndex: number;
+  pronunciation: PronunciationConfig[];
+};
 
 export type LanguagePronunciationMap = {
-  [key in LanguageType]: LanguagePronunciationMapConfig
-}
+  [key in LanguageType]: LanguagePronunciationMapConfig;
+};
 
 export type SoundResource = {
-  key: string
-  name: string
-  filename: string
-}
+  key: string;
+  name: string;
+  filename: string;
+};
 
-export const SOUND_URL_PREFIX = './sounds/'
-export const KEY_SOUND_URL_PREFIX = SOUND_URL_PREFIX + 'key-sound/'
+export const SOUND_URL_PREFIX = import.meta.env.BASE_URL + 'sounds/';
+export const KEY_SOUND_URL_PREFIX = SOUND_URL_PREFIX + 'key-sound/';
 
 const videoList = import.meta.glob(['../../../public/sounds/key-sound/*.(wav|mp3)'], {
   eager: false,
-})
+});
 
 /**
  * The mechanical keyboard sounds come from https://github.com/tplai/kbsim.
  */
 export const keySoundResources: SoundResource[] = Object.keys(videoList)
   .map((k) => {
-    const name = k.replace(/(.*\/)*([^.]+).*/gi, '$2')
-    const suffix = k.substring(k.lastIndexOf('.'))
+    const name = k.replace(/(.*\/)*([^.]+).*/gi, '$2');
+    const suffix = k.substring(k.lastIndexOf('.'));
     return {
       key: name,
       name: `${name}`,
       filename: `${name}${suffix}`,
-    }
+    };
   })
   .sort((a, b) => {
     if (a.key === 'Default') {
-      return -1
+      return -1;
     }
     if (b.key === 'Default') {
-      return 1
+      return 1;
     }
 
-    return a.key.localeCompare(b.key)
-  })
+    return a.key.localeCompare(b.key);
+  });
 
-export const wrongSoundResources: SoundResource[] = [{ key: '1', name: 'Sound 1', filename: 'beep.wav' }]
+export const wrongSoundResources: SoundResource[] = [{ key: '1', name: 'Sound 1', filename: 'beep.wav' }];
 
-export const correctSoundResources: SoundResource[] = [{ key: '1', name: 'Sound 1', filename: 'correct.wav' }]
+export const correctSoundResources: SoundResource[] = [{ key: '1', name: 'Sound 1', filename: 'correct.wav' }];
 
 export const LANG_PRON_MAP: LanguagePronunciationMap = {
   en: {
@@ -146,4 +145,4 @@ export const LANG_PRON_MAP: LanguagePronunciationMap = {
       },
     ],
   },
-}
+};
