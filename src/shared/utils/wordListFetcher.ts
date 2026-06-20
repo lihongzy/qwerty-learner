709b6f073310d616e5bc349a4f3824ba8cdfa22c
lihongzy <l1810638451@gmail.com>
2026-06-20 13:35:01 +0800

    fix: 使用环境变量配置URL前缀

--- src/shared/utils/wordListFetcher.ts
+++ src/shared/utils/wordListFetcher.ts
@@ -1,10 +1,10 @@
-import { Word } from "@/shared/types"
+import { Word } from '@/shared/types';
 
-export async function wordListFetcher(url:string) :Promise<Word[]>{
-    const URL_PREFIX :string = ''
+export async function wordListFetcher(url: string): Promise<Word[]> {
+  const URL_PREFIX = import.meta.env.BASE_URL;
 
-    const response = await fetch(URL_PREFIX+url)
-    const words:Word[] = await response.json()
+  const response = await fetch(URL_PREFIX + url);
+  const words: Word[] = await response.json();
 
-    return words
+  return words;
 }
